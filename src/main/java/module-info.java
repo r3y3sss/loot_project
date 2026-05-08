ccef31643525e11d1e555718b871b67bba20b804
carlos reyes <carlos1016reyes@gmail.com>
2026-05-07 23:10:46 -0700

    Primer commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.loot_proyect {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.compiler;
 
     opens com.example.loot_proyect to javafx.fxml;
     exports com.example.loot_proyect;
