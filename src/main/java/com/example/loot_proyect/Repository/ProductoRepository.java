d30b37dfa3b02269e6499650370d70f9a45d96d2
URIG10D <24380039@loscabos.tecnm.mx>
2026-05-11 21:20:30 -0700

    Add files via upload

---
+++ src/main/java/com/example/loot_proyect/Repository/ProductoRepository.java
@@ -0,0 +1,55 @@
+package com.example.loot_proyect.utils.Repository;
+
+import com.example.loot_proyect.model.ProductoEntity;
+import com.example.loot_proyect.utils.HibernateUtils;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
+
+public class ProductoRepository {
+
+
+    public void AgregarProducto(ProductoEntity Producto) {
+        EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(Producto);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+
+    }
+    public void EliminarProducto(ProductoEntity Producto) {
+        EntityManager entityManager= HibernateUtils.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(entityManager.merge(Producto));
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+    }
+    public void ActualizarProducto(ProductoEntity Producto) {
+        EntityManagerFactory entityManagerFactory = HibernateUtils.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.merge(Producto);
+        entityManager.getTransaction().commit();
+
+    }
+    public List<ProductoEntity> buscarProductos() {
+        EntityManagerFactory entityManagerFactory = HibernateUtils.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        List<ProductoEntity> lista =entityManager.createQuery("from ProductoEntity").getResultList();
+        entityManager.close();
+        return lista;
+    }
+    public ProductoEntity buscarIdProducto(int idProducto) {
+        EntityManagerFactory entityManagerFactory = HibernateUtils.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        ProductoEntity productoEntity = entityManager.find(ProductoEntity.class, idProducto);
+        entityManager.close();
+        return productoEntity;
+    }
+
+}
+
