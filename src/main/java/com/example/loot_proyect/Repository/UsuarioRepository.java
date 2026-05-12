175a09acc05f940598866e16ac9a7286ab97e279
URIG10D <24380039@loscabos.tecnm.mx>
2026-05-11 21:58:22 -0700

    Add files via upload

--- src/main/java/com/example/loot_proyect/Repository/UsuarioRepository.java
+++ src/main/java/com/example/loot_proyect/Repository/UsuarioRepository.java
@@ -1,4 +1,31 @@
 package com.example.loot_proyect.Repository;
 
+import com.example.loot_proyect.model.UsuarioEntity;
+import com.example.loot_proyect.utils.HibernateUtils;
+import jakarta.persistence.EntityManager;
+
 public class UsuarioRepository {
+
+    public void agregarUsuario(UsuarioEntity usuario) {
+        EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(usuario);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+    public void eliminarUsuario(UsuarioEntity usuario) {
+        EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(usuario);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+    public void actualizarUsuario(UsuarioEntity usuario) {
+        EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.merge(usuario);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+    
 }
