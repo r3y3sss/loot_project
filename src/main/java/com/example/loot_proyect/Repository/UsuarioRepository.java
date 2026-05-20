589a38758100a349f185d323b64ddc207fb44f8b
URIG10D <24380039@loscabos.tecnm.mx>
2026-05-19 22:16:32 -0700

    Add files via upload

--- src/main/java/com/example/loot_proyect/Repository/UsuarioRepository.java
+++ src/main/java/com/example/loot_proyect/Repository/UsuarioRepository.java
@@ -8,24 +8,76 @@ public class UsuarioRepository {
 
     public void agregarUsuario(UsuarioEntity usuario) {
         EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.persist(usuario);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(usuario);
+        }catch(Exception e) {}
     }
     public void eliminarUsuario(UsuarioEntity usuario) {
+
         EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
         entityManager.getTransaction().begin();
-        entityManager.remove(usuario);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        try{
+            usuario = entityManager.find(UsuarioEntity.class, usuario.getId());
+            entityManager.getTransaction().commit();
+
+           if (usuario != null) {
+               entityManager.remove(usuario);
+           }else {
+            entityManager.merge(usuario);
+           }
+
+        }catch (Exception ex){
+            entityManager.getTransaction().rollback();
+        }finally {
+            entityManager.close();
+        }
     }
     public void actualizarUsuario(UsuarioEntity usuario) {
         EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.merge(usuario);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.merge(usuario);
+            entityManager.getTransaction().commit();
+
+        }catch(Exception ex){
+            if(entityManager.getTransaction().isActive()){
+                entityManager.getTransaction().rollback();
+            }
+            System.err.println("Error al actualizar el usuario "+ex.getMessage());
+            ex.printStackTrace();
+
+        }finally{
+            if(entityManager != null && entityManager.isOpen()){
+                entityManager.close();
+            }
+        }
+
+    }
+
+    public UsuarioEntity buscarUsuario(UsuarioEntity usuario) {
+        EntityManager entityManager = HibernateUtils.getEntityManagerFactory().createEntityManager();
+        UsuarioEntity usuarioEncontrado = null;
+
+        try {
+            usuarioEncontrado = entityManager.find(UsuarioEntity.class, usuario.getId());
+            if (usuarioEncontrado != null) {
+                if(usuarioEncontrado.getDireccionEntity() != null){
+                    usuarioEncontrado.getDireccionEntity().hashCode();
+                }
+            }
+
+        } catch (Exception ex) {
+            System.err.println("Error al buscar el usuario"+ex.getMessage());
+            ex.printStackTrace();
+
+        }finally {
+            if (entityManager != null && entityManager.isOpen()) {
+                entityManager.close();
+            }
+        }
+
+        return usuarioEncontrado;
     }
 
 }
